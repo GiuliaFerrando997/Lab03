dc318cffa53b438f4bc6f017a8762bc1b0a1354e
Giulia <Giulia@172.21.56.77>
2019-03-27 16:22:07 +0100

    Modificato il main per MVC

---
+++ Lab3_SpellChecker/src/it/polito/tdp/SpellCheckerModel/SpellCheckerModel.java
@@ -0,0 +1,5 @@
+package it.polito.tdp.SpellCheckerModel;
+
+public class SpellCheckerModel {
+
+}
--- Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
+++ Lab3_SpellChecker/src/it/polito/tdp/spellchecker/controller/SpellCheckerController.java
@@ -1,5 +1,15 @@
 package it.polito.tdp.spellchecker.controller;
 
+import it.polito.tdp.SpellCheckerModel.SpellCheckerModel;
+
 public class SpellCheckerController {
+	
+	SpellCheckerModel elenco;
+	private SpellCheckerModel model;
+	
+	public void setModel(SpellCheckerModel model) {
+		this.model = model;
+	}
+	
 	
 }
